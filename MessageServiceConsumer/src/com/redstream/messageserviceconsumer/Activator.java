4e8e80479af94855f7bfc0d78484dace537e445b
Navod <54998118+navodveduth@users.noreply.github.com>
2023-03-17 21:23:56 +0530

    minor changes

--- MessageServiceConsumer/src/com/redstream/messageserviceconsumer/Activator.java
+++ MessageServiceConsumer/src/com/redstream/messageserviceconsumer/Activator.java
@@ -45,6 +45,7 @@ public class Activator implements BundleActivator {
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+		System.out.println("LOG - Message Consumer Stopped!");
 	}
 
 }
